d9791abd8dc5537aed1252ff34fb89092aaae067
Dasith Kuruppu <DasithK@embla.lk>
2018-12-12 13:42:20 +0530

    typings added

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
+import { CroppieOptions, ResultOptions } from "croppie";
 const Croppie = require("croppie").Croppie; // this is done to fix an issue with the default typings of croppie
+export interface CroppieReactOpts extends CroppieOptions {
+  result?: object;
+}
 
-export type Props = {
-  Options: object;
-  className: string;
-  styles: object;
-  result: object;
-  OnUpdate: (event: any) => void;
-};
+export interface Props {
+  Options: CroppieReactOpts;
+  className?: string;
+  styles?: object;
+  result?: ResultOptions;
+  OnUpdate?: (event: any) => void;
+}
 
-export default class CroppieWrapper extends React.Component<Props, any> {
+export interface State {
+  croppieInstance: any;
+  className?: string;
+  styles?: object;
+  result?: ResultOptions;
+}
+
+export default class CroppieWrapper extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = { croppieInstance: null };
@@ -21,6 +32,7 @@ export default class CroppieWrapper extends React.Component<Props, any> {
       className,
       styles,
       result = { type: "blob" },
+      children,
       ...other
     } = this.props;
     this.setState(
@@ -32,7 +44,7 @@ export default class CroppieWrapper extends React.Component<Props, any> {
         className,
         styles,
         result
-      },
+      } as State,
       () => {
         this.state.croppieInstance.bind(other);
         document
@@ -42,12 +54,14 @@ export default class CroppieWrapper extends React.Component<Props, any> {
     );
   }
 
-  croppieUpdated(event: any) {
+  croppieUpdated(event: Croppie.CropData) {
     if (this.state.croppieInstance) {
-      this.state.croppieInstance.result(this.state.result).then((blob: any) => {
-        event.result = blob;
-        return this.props.OnUpdate(event);
-      });
+      this.state.croppieInstance
+        .result(this.state.result)
+        .then((blob: Blob) => {
+          event["result"] = blob;
+          return this.props.OnUpdate(event);
+        });
     } else {
       return this.props.OnUpdate(event);
     }
